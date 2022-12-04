71b7639953248e0460b5029275cbc973de00543e
tatiana-levenets <119804581+tatiana-levenets@users.noreply.github.com>
2022-12-04 19:33:46 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 4, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e3',
+    title: 'Towel',
+    amount: 120,
+    date: new Date(2022, 1, 8),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2022, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Book',
+    amount: 58,
+    date: new Date(2022, 11, 1),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
